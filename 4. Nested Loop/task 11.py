8487bc0bbd4bc9a58229cb775dfd2039ed1116b0
zainibsohail-jpg <zainibsohail@gmail.com>
2026-06-07 01:02:53 +0500

    Add files via upload

---
+++ 4. Nested Loop/task 11.py
@@ -0,0 +1,13 @@
+i=1
+while i <= 4:
+    j=0
+    while(j<=4-i):
+        print(" ", end=" ")
+        j=j+1
+    j=0
+    while j<(2*i)-1:
+        print("*", end=" ")
+        j=j+1
+    print( )
+    i=i+1
+
