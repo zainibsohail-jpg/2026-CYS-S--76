033a2743daf51df500c7db991b66211b02103fa3
zainibsohail-jpg <zainibsohail@gmail.com>
2026-06-07 01:02:09 +0500

    Add files via upload

---
+++ 3. while loop/task 4.py
@@ -0,0 +1,27 @@
+y=int(input("enter number of students"))
+i=0
+while (i<=50):
+    n=input("enter your name")
+    a=int(input("enter obtained marks"))
+    b=int(input("enter total marks"))
+    c=a/b*100
+    print(c)
+    if c>=90:
+        print("A")
+    elif c>=85:
+        print("A-")
+    elif c>=80:
+        print("B+")
+    elif c>=75:
+        print("B-")
+    elif c>=70:
+        print("C+")
+    elif c>=65:
+        print("C-")
+    elif c>=60:
+        print("D+")
+    elif c>=55:
+        print("D-")
+    else :
+        print("F")
+i=i+1
