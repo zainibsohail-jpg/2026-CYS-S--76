7acfbe45bd74e664e670140fa13999e05c72e423
zainibsohail-jpg <zainibsohail@gmail.com>
2026-06-07 19:20:30 +0500

    Add files via upload

---
+++ Week 2. for Loop/task 1.py
@@ -0,0 +1,8 @@
+binary = input("Enter a binary number: ")
+decimal = 0
+power = 0
+for digit in reversed(binary):
+    decimal += int(digit) * (2 ** power)
+    power += 1
+
+print("Decimal Equivalent =", decimal)
