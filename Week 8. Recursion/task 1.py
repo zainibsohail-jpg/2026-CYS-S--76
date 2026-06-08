1e289b75a56b4dae5f41688ce8153e92b0276a61
zainibsohail-jpg <zainibsohail@gmail.com>
2026-06-08 22:20:03 +0500

    Delete Week 8. Recursion directory

--- Week 8. Recursion/task 1.py
+++
@@ -1,5 +0,0 @@
-def n (a,b):
-    s = a+b
-    return s
-v=n (1,2)
-print(v)
--- Week 8. Recursion/task 3.py
+++
@@ -1,6 +0,0 @@
-def show(n):
-    if (n==0) or (n==1):
-        return 1
-    else:
-        return show(n - 1)
-print(show(6))
